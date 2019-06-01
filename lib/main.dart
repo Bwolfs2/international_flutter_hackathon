6cac500981095c0385a5e949f43d97bd62aeffa6
David Araujo <davidsdearaujo@gmail.com>
2019-06-01 14:46:44 -0300

    Adicionado BottomBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:international_flutter_hackathon/src/screens/initial/initial_bloc.dart';
+import 'package:international_flutter_hackathon/src/screens/initial/initial_screen.dart';
 import 'src/screens/home/home_screen.dart';
 
 void main() => runApp(MyApp());
@@ -7,12 +9,32 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      blocs: [
+        Bloc((i) => InitialBloc()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primaryColor: Color(0xff3AD29F),
+          accentColor: Color(0xff736ff4),
+          bottomAppBarColor: Colors.grey[200],
+          appBarTheme: AppBarTheme(
+            textTheme: TextTheme(
+              title: TextStyle(
+                color: Colors.white70,
+                fontSize: 20,
+                fontFamily: "Lato",
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          textTheme: TextTheme(
+            title: TextStyle(color: Colors.white),
+          ),
+        ),
+        home: InitialScreen(),
       ),
-      home: HomeScreen(),
     );
   }
 }
---
+++ lib/src/screens/initial/initial_bloc.dart
@@ -0,0 +1,10 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:flutter/material.dart';
+
+class InitialBloc extends BlocBase{
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
