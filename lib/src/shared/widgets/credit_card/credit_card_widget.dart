b9988415f173f5680f9c70ca114138376e278b53
David Araujo <davidsdearaujo@gmail.com>
2019-06-01 12:33:56 -0300

    Initial Commit

---
+++ lib/src/shared/widgets/credit_card/credit_card_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CreditCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        width: 250,
+        height: 150,
+      ),
+    );
+  }
+}
