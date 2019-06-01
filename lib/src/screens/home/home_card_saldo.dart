833f2b1a40246360abb12a6f9f029e50a861e883
Vilson Dauinheimer <bwolfnoob@gmail.com>
2019-06-01 15:07:43 -0300

    tela principal

---
+++ lib/src/screens/home/home_card_saldo.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class HomeCardSaldo extends StatefulWidget {
+  @override
+  _HomeCardSaldoState createState() => _HomeCardSaldoState();
+}
+
+class _HomeCardSaldoState extends State<HomeCardSaldo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Color(0xff736ff4),
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      width: MediaQuery.of(context).size.width,
+      height: 180,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children: <Widget>[
+                Text(
+                  "SALDO",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 30),
+                ),
+                Text(
+                  "JUN/19",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w200,
+                      fontSize: 27),
+                ),
+              ],
+            ),
+          ),
+
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text("R\$ 270,00" , style: TextStyle(
+                  fontWeight: FontWeight.w700, color: Color(0xfff2f2f2) , fontSize: 45
+                ),),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
