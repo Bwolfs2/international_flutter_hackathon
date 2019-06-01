72b28df26242483d79a4c5616802496c965bf4e3
Vilson Dauinheimer <bwolfnoob@gmail.com>
2019-06-01 15:07:55 -0300

    fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
-
-import 'src/screens/home/home_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:international_flutter_hackathon/src/screens/home/home_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'BSYNC',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primaryColor: Color(0xff3AD29F),
+          accentColor: Color(0xff736ff4),
+          appBarTheme: AppBarTheme(
+              textTheme: TextTheme(
+                  title: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 20,
+                      fontFamily: "Lato",
+                      fontWeight: FontWeight.w400))),
+          textTheme: TextTheme(title: TextStyle(color: Colors.white))),
       home: HomeScreen(),
     );
   }
---
+++ lib/src/screens/home/home_screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:animated_card/animated_card.dart';
+import 'home_card_balanco.dart';
+import 'home_card_gastos.dart';
+import 'home_card_saldo.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: PreferredSize(
+          child: Container(
+            color: Color(0xfff2f2f2),
+            padding: const EdgeInsets.only(left: 20, top: 10),
+            child: Row(
+              children: <Widget>[
+                CircleAvatar(
+                    child: Icon(
+                      Icons.person,
+                      color: Colors.white,
+                    ),
+                    backgroundColor: Color(0xff736ff4)),
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Text("Bem vindo,"),
+                      Text(
+                        "Pedro da Silva",
+                        style:
+                            TextStyle(color: Color(0xff30c7b8), fontSize: 18),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          preferredSize: const Size.fromHeight(70),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              AnimatedCard(
+                direction: AnimatedCardDirection.left,
+                //Initial animation direction
+                initDelay: Duration(milliseconds: 0),
+                //Delay to initial animation
+                duration: Duration(seconds: 1),
+                //Initial animation duration
+                child: HomeCardGastos(),
+              ),
+              AnimatedCard(
+                direction: AnimatedCardDirection.right,
+                //Initial animation direction
+                initDelay: Duration(milliseconds: 0),
+                //Delay to initial animation
+                duration: Duration(seconds: 1),
+                //Initial animation duration
+                child: HomeCardSaldo(),
+              ),
+              AnimatedCard(
+                direction: AnimatedCardDirection.bottom,
+                //Initial animation direction
+                initDelay: Duration(milliseconds: 0),
+                //Delay to initial animation
+                duration: Duration(seconds: 1),
+                //Initial animation duration
+                child: HomeCardBalanco(),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
